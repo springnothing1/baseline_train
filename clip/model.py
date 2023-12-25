7fccfc3850c864c9ad2ec157f916e28deff8801e
mlq <1743406496@qq.com>
2023-12-25 15:37:35 +0800

    perfict clip running with high accuracy

--- clip/model.py
+++ clip/model.py
@@ -360,22 +360,11 @@ class CLIP(nn.Module):
     def forward(self, image, text):
         image_features = self.encode_image(image)
         text_features = self.encode_text(text)
-
-        # normalized features
-        #image_features = image_features / image_features.norm(dim=1, keepdim=True)
-        #text_features = text_features / text_features.norm(dim=1, keepdim=True)
         
-        logit_scale = self.logit_scale.exp()
-        all_feature = torch.cat((image_features, text_features), dim=1) * logit_scale
+        #logit_scale = self.logit_scale.exp()
+        all_feature = torch.cat((image_features, text_features), dim=1)
         
         return all_feature
-        """ # cosine similarity as logits
-        logit_scale = self.logit_scale.exp()
-        logits_per_image = logit_scale * image_features @ text_features.t()
-        logits_per_text = logits_per_image.t()
-
-        # shape = [global_batch_size, global_batch_size]
-        return logits_per_image, logits_per_text"""
 
 
 def convert_weights(model: nn.Module):
